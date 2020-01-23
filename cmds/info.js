2cd8620cabce99bc5af9267933ca4afea023f7ab
DexMP <matveymp@yandex.ru>
2020-01-23 13:53:04 +0300

    +cmds

---
+++ cmds/info.js
@@ -0,0 +1,15 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = (bot,message,args) => {
+    message.channel.send('');
+    let embed = new Discord.RichEmbed()
+    .setDescription("Информация о боте")
+    .setColor('#ffcc00')
+    .addField("Ссылка на бота", "https://discordapp.com/oauth2/authorize?client_id=620545059837706276&permissions=8&scope=bot")
+    .addField("Разработчик", "<@288618868686192640>")
+    .addField("Донат", "https://money.yandex.ru/to/410015720722325");
+    message.channel.send(embed);
+};
+module.exports.help = {
+    name: "info"
+};
